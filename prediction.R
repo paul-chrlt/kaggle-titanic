a77624ef2fa649978bb9258890d2dfcde8325465
paul-chrlt <paul.charlet@gmail.com>
2019-07-15 22:50:21 +0200

    kaggle file creation

--- prediction.R
+++ prediction.R
@@ -5,7 +5,7 @@ library(caret)
 ## data import
 
 trainSource <-read.csv("./datasource/train.csv",stringsAsFactors = FALSE) 
-testSource <- read.csv("./datasource/test.csv",stringsAsFactors = FALSE)
+kaggletestSource <- read.csv("./datasource/test.csv",stringsAsFactors = FALSE)
 
 ## Splitting dataset
 
@@ -16,10 +16,12 @@ testset <- trainSource[-trainindex,]
 ## data cleaning
 
 meanage <- mean(trainSource$Age,na.rm = TRUE)
+meanfare <- mean(trainSource$Fare,na.rm=TRUE)
 
 formatter <- function(dataset){
     dataset$Survived <- as.factor(dataset$Survived)
     dataset[is.na(dataset$Age),]$Age <- meanage
+    dataset[is.na(dataset$Fare),]$Fare <- meanfare
     dataset$Pclass <- as.factor(dataset$Pclass)
     dataset$Sex <- as.factor(dataset$Sex)
     dataset$Embarked <- as.factor(dataset$Embarked)
@@ -28,6 +30,7 @@ formatter <- function(dataset){
 
 trainset <- formatter(trainset)
 testset <- formatter(testset)
+kaggletest <- formatter(kaggletestSource)
 
 ## feature engineering
 
@@ -42,16 +45,17 @@ featurecrator <- function(dataset){
     dataset$hasPartner <- dataset$SibSp > 0
     ### name length
     dataset$nameLength <- str_length(dataset$Name)
+    ### has a cabin
+    dataset$hasCabin <- dataset$Cabin != ""
     dataset
 }
 
 trainset <- featurecrator(trainset)
 testset <- featurecrator(testset)
-
+kaggletest <- featurecrator(kaggletest)
 
 ### dataset preparation
 
-outcome <- trainset$Survived
 modeltrainset <- trainset[,c(2,3,5,6,7,8,10,12,13,14,15,16)]
 
 traincontrol <- trainControl()
@@ -59,24 +63,25 @@ traincontrol <- trainControl()
 
 ### GLM, 82% accuracy
 
-glmmodel <- glm(Survived~.,data = modeltrainset,family = "binomial")
+glmmodel <- glm(Survived~Pclass+Sex+Age+SibSp+Parch+Fare+Embarked+isChild+hasFamily+hasPartner+nameLength+hasCabin,data = trainset,family = "binomial")
 glmprediction <- predict(glmmodel,testset)
 glmprediction[glmprediction>.5] <- 1
 glmprediction[glmprediction<.5] <- 0
 confusionMatrix(as.factor(glmprediction),as.factor(testset$Survived))
 
 ### adaBoost, 82% accuracy
-adaboostModel <- train(modeltrainset[,-1],
-                    outcome,
-                    method = "adaboost")
+adaboostModel <- train(Survived~Pclass+Sex+Age+SibSp+Parch+Fare+Embarked+isChild+hasFamily+hasPartner+nameLength+hasCabin,
+                    method = "adaboost",
+                    data=trainset)
 adaboostprediction <- predict(adaboostModel,testset)
 confusionMatrix(adaboostprediction,testset$Survived)
 
 ### Random Forest, 85% accuracy
 
-rfModel <- train(modeltrainset[,-1],
-                 outcome,
-                 method = "rf")
+rfModel <- train(Survived~Pclass+Sex+Age+SibSp+Parch+Fare+Embarked+isChild+hasFamily+hasPartner+nameLength+hasCabin,
+                 method="rf",
+                 data = trainset)
+
 rfPrediction <- predict(rfModel,testset)
 confusionMatrix(rfPrediction,testset$Survived)
 
@@ -88,3 +93,20 @@ predictionsVote$rfPrediction <- as.integer(as.character(predictionsVote$rfPredic
 predictionsVote$vote <- as.integer((predictionsVote$glmprediction+predictionsVote$adaboostprediction+predictionsVote$rfPrediction)>1)
 
 confusionMatrix(as.factor(predictionsVote$vote),testset$Survived)
+
+## Predict Kaggle data
+kglmprediction <- predict(glmmodel,kaggletest)
+kglmprediction[kglmprediction>.5] <- 1
+kglmprediction[kglmprediction<.5] <- 0
+
+kadaboostprediction <- predict(adaboostModel,kaggletest)
+
+krfPrediction <- predict(rfModel,kaggletest)
+
+kpredictionsVote <- data.frame(kglmprediction,kadaboostprediction,krfPrediction)
+kpredictionsVote$kadaboostprediction <- as.integer(as.character(kpredictionsVote$kadaboostprediction))
+kpredictionsVote$krfPrediction <- as.integer(as.character(kpredictionsVote$krfPrediction))
+kpredictionsVote$vote <- as.integer((kpredictionsVote$kglmprediction+kpredictionsVote$kadaboostprediction+kpredictionsVote$krfPrediction)>1)
+
+ksubmission <- cbind(kaggletest$PassengerId,kpredictionsVote$vote)
+write.csv(ksubmission,file="submission.csv",row.names = FALSE)
